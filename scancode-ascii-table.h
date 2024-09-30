799925bb7252cb5e70790ee6ad26aacd7c86d349
Ângelo Azevedo <66081804+ang3lo-azevedo@users.noreply.github.com>
2024-09-30 11:28:44 +0100

    Update scancode-ascii-table.h

--- scancode-ascii-table.h
+++ scancode-ascii-table.h
@@ -4,131 +4,69 @@
 // remaining 7 bits are to be used as scan code number.
 
 const unsigned char ascii_to_scan_code_table[] PROGMEM = {
-  // /* ASCII:   0 */ 0,
-  // /* ASCII:   1 */ 0,
-  // /* ASCII:   2 */ 0,
-  // /* ASCII:   3 */ 0,
-  // /* ASCII:   4 */ 0,
-  // /* ASCII:   5 */ 0,
-  // /* ASCII:   6 */ 0,
-  // /* ASCII:   7 */ 0,
-  /* ASCII:   8 */ 42,
-  /* ASCII:   9 */ 43,
-  /* ASCII:  10 */ 40,
-  /* ASCII:  11 */ 0,
-  /* ASCII:  12 */ 0,
-  /* ASCII:  13 */ 0,
-  /* ASCII:  14 */ 0,
-  /* ASCII:  15 */ 0,
-  /* ASCII:  16 */ 0,
-  /* ASCII:  17 */ 0,
-  /* ASCII:  18 */ 0,
-  /* ASCII:  19 */ 0,
-  /* ASCII:  20 */ 0,
-  /* ASCII:  21 */ 0,
-  /* ASCII:  22 */ 0,
-  /* ASCII:  23 */ 0,
-  /* ASCII:  24 */ 0,
-  /* ASCII:  25 */ 0,
-  /* ASCII:  26 */ 0,
-  /* ASCII:  27 */ 41,
-  /* ASCII:  28 */ 0,
-  /* ASCII:  29 */ 0,
-  /* ASCII:  30 */ 0,
-  /* ASCII:  31 */ 0,
-  /* ASCII:  32 */ 44,
-  /* ASCII:  33 */ 158,
-  /* ASCII:  34 */ 159,
-  /* ASCII:  35 */ 116,
-  /* ASCII:  36 */ 161,
-  /* ASCII:  37 */ 162,
-  /* ASCII:  38 */ 163,
-  /* ASCII:  39 */ 45,
-  /* ASCII:  40 */ 165,
-  /* ASCII:  41 */ 166,
-  /* ASCII:  42 */ 176,
-  /* ASCII:  43 */ 48,
-  /* ASCII:  44 */ 54,
-  /* ASCII:  45 */ 56,
-  /* ASCII:  46 */ 55,
-  /* ASCII:  47 */ 164,
-  /* ASCII:  48 */ 39,
-  /* ASCII:  49 */ 30,
-  /* ASCII:  50 */ 31,
-  /* ASCII:  51 */ 32,
-  /* ASCII:  52 */ 33,
-  /* ASCII:  53 */ 34,
-  /* ASCII:  54 */ 35,
-  /* ASCII:  55 */ 36,
-  /* ASCII:  56 */ 37,
-  /* ASCII:  57 */ 38,
-  /* ASCII:  58 */ 183,
-  /* ASCII:  59 */ 182,
-  /* ASCII:  60 */ 100,
-  /* ASCII:  61 */ 167,
-  /* ASCII:  62 */ 228,
-  /* ASCII:  63 */ 173,
-  /* ASCII:  64 */ 115,
-  /* ASCII:  65 */ 132,
-  /* ASCII:  66 */ 133,
-  /* ASCII:  67 */ 134,
-  /* ASCII:  68 */ 135,
-  /* ASCII:  69 */ 136,
-  /* ASCII:  70 */ 137,
-  /* ASCII:  71 */ 138,
-  /* ASCII:  72 */ 139,
-  /* ASCII:  73 */ 140,
-  /* ASCII:  74 */ 141,
-  /* ASCII:  75 */ 142,
-  /* ASCII:  76 */ 143,
-  /* ASCII:  77 */ 144,
-  /* ASCII:  78 */ 145,
-  /* ASCII:  79 */ 146,
-  /* ASCII:  80 */ 147,
-  /* ASCII:  81 */ 148,
-  /* ASCII:  82 */ 149,
-  /* ASCII:  83 */ 150,
-  /* ASCII:  84 */ 151,
-  /* ASCII:  85 */ 152,
-  /* ASCII:  86 */ 153,
-  /* ASCII:  87 */ 154,
-  /* ASCII:  88 */ 155,
-  /* ASCII:  89 */ 156,
-  /* ASCII:  90 */ 157,
-  /* ASCII:  91 */ 175,
-  /* ASCII:  92 */ 53,
-  /* ASCII:  93 */ 176,
-  /* ASCII:  94 */ 174,
-  /* ASCII:  95 */ 184,
-  /* ASCII:  96 */ 0,
-  /* ASCII:  97 */ 4,
-  /* ASCII:  98 */ 5,
-  /* ASCII:  99 */ 6,
-  /* ASCII: 100 */ 7,
-  /* ASCII: 101 */ 8,
-  /* ASCII: 102 */ 9,
-  /* ASCII: 103 */ 10,
-  /* ASCII: 104 */ 11,
-  /* ASCII: 105 */ 12,
-  /* ASCII: 106 */ 13,
-  /* ASCII: 107 */ 14,
-  /* ASCII: 108 */ 15,
-  /* ASCII: 109 */ 16,
-  /* ASCII: 110 */ 17,
-  /* ASCII: 111 */ 18,
-  /* ASCII: 112 */ 19,
-  /* ASCII: 113 */ 20,
-  /* ASCII: 114 */ 21,
-  /* ASCII: 115 */ 22,
-  /* ASCII: 116 */ 23,
-  /* ASCII: 117 */ 24,
-  /* ASCII: 118 */ 25,
-  /* ASCII: 119 */ 26,
-  /* ASCII: 120 */ 27,
-  /* ASCII: 121 */ 28,
-  /* ASCII: 122 */ 29,
-  /* ASCII: 123 */ 431,
-  /* ASCII: 124 */ 181,
-  /* ASCII: 125 */ 432,
-  /* ASCII: 126 */ 0
+  /* ASCII:   8 */ 42,      // Backspace
+  /* ASCII:   9 */ 43,      // Tab
+  /* ASCII:  10 */ 40,      // Enter
+  /* ASCII:  27 */ 41,      // Escape
+  /* ASCII:  32 */ 44,      // Space
+  /* ASCII:  33 */ 30 | 0x80,  // ! (Shift + 1)
+  /* ASCII:  34 */ 31 | 0x80,  // " (Shift + 2)
+  /* ASCII:  35 */ 32,      // # (AltGr + 3 no PT)
+  /* ASCII:  36 */ 33,      // $ (AltGr + 4 no PT)
+  /* ASCII:  37 */ 34 | 0x80,  // % (Shift + 5)
+  /* ASCII:  38 */ 35 | 0x80,  // & (Shift + 6)
+  /* ASCII:  39 */ 45,      // ' (Apostrophe)
+  /* ASCII:  40 */ 37 | 0x80,  // ( (Shift + 8)
+  /* ASCII:  41 */ 38 | 0x80,  // ) (Shift + 9)
+  /* ASCII:  42 */ 48 | 0x80,  // * (Shift + +)
+  /* ASCII:  43 */ 48,      // + 
+  /* ASCII:  44 */ 54,      // , 
+  /* ASCII:  45 */ 56,      // -
+  /* ASCII:  46 */ 55,      // .
+  /* ASCII:  47 */ 164,     // /
+  /* ASCII:  48 */ 39,      // 0
+  /* ASCII:  49 */ 30,      // 1
+  /* ASCII:  50 */ 31,      // 2
+  /* ASCII:  51 */ 32,      // 3
+  /* ASCII:  52 */ 33,      // 4
+  /* ASCII:  53 */ 34,      // 5
+  /* ASCII:  54 */ 35,      // 6
+  /* ASCII:  55 */ 36,      // 7
+  /* ASCII:  56 */ 37,      // 8
+  /* ASCII:  57 */ 38,      // 9
+  /* ASCII:  64 */ 31 | 0x80,  // @ (AltGr + 2)
+  /* ASCII:  91 */ 26 | 0x80,  // [ (AltGr + 8 no PT)
+  /* ASCII:  92 */ 53,      // \
+  /* ASCII:  93 */ 27 | 0x80,  // ] (AltGr + 9 no PT)
+  /* ASCII: 123 */ 47 | 0x80,  // { (AltGr + Shift + 8 no PT)
+  /* ASCII: 124 */ 53 | 0x80,  // | (AltGr + < no PT)
+  /* ASCII: 125 */ 48 | 0x80,  // } (AltGr + Shift + 9 no PT)
+  /* ASCII: 126 */ 49 | 0x80,  // ~ (Shift + AltGr + 4)
+  /* ASCII:  97 */ 4,       // a
+  /* ASCII:  98 */ 5,       // b
+  /* ASCII:  99 */ 6,       // c
+  /* ASCII: 100 */ 7,       // d
+  /* ASCII: 101 */ 8,       // e
+  /* ASCII: 102 */ 9,       // f
+  /* ASCII: 103 */ 10,      // g
+  /* ASCII: 104 */ 11,      // h
+  /* ASCII: 105 */ 12,      // i
+  /* ASCII: 106 */ 13,      // j
+  /* ASCII: 107 */ 14,      // k
+  /* ASCII: 108 */ 15,      // l
+  /* ASCII: 109 */ 16,      // m
+  /* ASCII: 110 */ 17,      // n
+  /* ASCII: 111 */ 18,      // o
+  /* ASCII: 112 */ 19,      // p
+  /* ASCII: 113 */ 20,      // q
+  /* ASCII: 114 */ 21,      // r
+  /* ASCII: 115 */ 22,      // s
+  /* ASCII: 116 */ 23,      // t
+  /* ASCII: 117 */ 24,      // u
+  /* ASCII: 118 */ 25,      // v
+  /* ASCII: 119 */ 26,      // w
+  /* ASCII: 120 */ 27,      // x
+  /* ASCII: 121 */ 28,      // y
+  /* ASCII: 122 */ 29,      // z
+  /* ASCII: 231 */ 51,      // ç (Portuguese key)
 };
